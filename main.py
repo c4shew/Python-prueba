cd92bc9f08f1af0e383d0e0fb1b417ac18457629
c4shew <constanzabianco@outlook.com>
2021-11-14 22:53:13 -0300

    probando

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+x = 0
+while x < 10:
+    x = int (input("Enter a number:"))
+    print (x)
+
+
+print("Mis primeras lineas de Python :)")
+
+'''Verificar munero de dos cifras'''
+num = int (input("Ingrese un número:"))
+if num>9 and num<100:
+    print ("El numero tiene dos cifras")
+
+
+
